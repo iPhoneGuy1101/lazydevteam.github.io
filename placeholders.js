032574c76977de045f42b2505737d751148b9878
Lazy DevTeam <lazy.devteam@gmail.com>
2017-06-24 13:40:05 -0500

    Update placeholders.js

--- placeholders.js
+++ placeholders.js
@@ -4,6 +4,7 @@ html.innerHTML = html.innerHTML.replace(/placeholder_devteam/g, "Developers");
 html.innerHTML = html.innerHTML.replace(/placeholder_projects/g, "Projects");
 html.innerHTML = html.innerHTML.replace(/placeholder_downloads/g, "Applications");
 html.innerHTML = html.innerHTML.replace(/placeholder_lg/g, "Lazy Guy Jimmy");
+html.innerHTML = html.innerHTML.replace(/placeholder_h4l/g, "Hacks4live");
 html.innerHTML = html.innerHTML.replace(/placeholder_ipg/g, "iPhoneGuy1101");
 html.innerHTML = html.innerHTML.replace(/urlbase/g, "");
 html.innerHTML = html.innerHTML.replace(/emoji_happy/g, "<img class='emoji' src='/emojis/happy.png' width=32 height=24 />");
